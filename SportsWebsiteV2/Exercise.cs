fcd32382268850e5ede5d19adfbab7a3c7d799d2
עמית דגני <gildag67@gmail.com>
2020-09-20 21:57:45 +0300

    ADD Exercise class and ExerciseList class

---
+++ SportsWebsiteV2/Exercise.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsWebsiteV2
+{
+    public class Exercise
+    {
+        public string Link;
+        public string Name;
+        public string Reps;
+        public string Sets;
+
+        public Exercise(string name, bool IsCompund)
+        {
+            this.Name = name;
+            this.Link = YTLinkGenerator(name);
+            RepsSetsGenerator(IsCompund);
+        }
+
+        private string YTLinkGenerator(string name)
+        {//bulid a yt link to the search of this exc name by adding the bane if only one word but if more it put + sign between the words
+            string link = " https://www.youtube.com/results?search_query=",
+                word = "";
+            bool first = true;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == ' ')
+                {
+                    if (!first)
+                    {
+                        link += "+" + word;
+                    }
+                    else
+                    {
+                        link += word;
+                        first = false;
+                    }
+                    word = "";
+                }
+                else
+                {
+                    word += name[i];
+                }
+            }
+            link += word;
+            return link;
+        }
+
+        private void RepsSetsGenerator(bool IsCompund)
+        {
+            if (IsCompund)
+            {
+                this.Reps = "6";
+                this.Sets = "4";
+            }
+            else
+            {
+                this.Reps = "12";
+                this.Sets = "3";
+            }
+        }
+    }
+}
---
+++ SportsWebsiteV2/ExerciseList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsWebsiteV2
+{
+    public class ExerciseList
+    {
+        public Dictionary<string, List<Exercise>> ExerciseDictionary;
+
+        public ExerciseList()
+        {
+            if (ExerciseDictionary == null)
+            {
+                ExerciseDictionary = new Dictionary<string, List<Exercise>>();
+            }
+        }
+
+        public void AddExercise(string MuscleGroup, string name, bool IsCompund)
+        {
+            Exercise exercise = new Exercise(name, IsCompund);
+            List<Exercise> ExcList = ExerciseDictionary[MuscleGroup];
+            ExcList.Add(exercise);
+            ExerciseDictionary[MuscleGroup] = ExcList;
+        }
+    }
+}
